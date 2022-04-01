aedd44690355e5a041b9cbfd97c03be5a7b11c02
admin <admin@YD5-10>
2022-04-01 09:33:23 +0900

    2022/04/01 CSS 적용 완료

--- midprj/src/main/java/com/midprj/member/command/MemberUpdatePwForm.java
+++ midprj/src/main/java/com/midprj/member/command/MemberUpdatePwForm.java
@@ -2,6 +2,7 @@ package com.midprj.member.command;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.midprj.comm.Command;
 
@@ -10,8 +11,15 @@ public class MemberUpdatePwForm implements Command {
 	@Override
 	public String exec(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
 		String fid = request.getParameter("fid");
-		request.setAttribute("fid", fid);
+		if(fid != null) {
+			request.setAttribute("fid", fid);			
+		}
+		else {
+			request.setAttribute("fid", session.getAttribute("loginId"));
+		}
+		
 		
 		return "member/memberUpdatePwForm";
 	}
